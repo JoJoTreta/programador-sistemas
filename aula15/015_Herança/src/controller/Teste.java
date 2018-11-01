1024ed0fd4ea72b644431fe4b704a2b9ea19a3de
JoJoTreta <44008077+JoJoTreta@users.noreply.github.com>
2018-11-01 19:01:41 -0300

    Add files via upload

---
+++ aula15/015_Herança/src/controller/Teste.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.Aluno;
+import model.Professor;
+
+public class Teste {
+	public static void main (String[] args) {
+		
+		Aluno aluno = new Aluno("Jonattan Diógenes", "22733067-7", "122120667-24", "21-99790-3013", "jonattan.jojo@gmail.com", "647136", "Programador de Sistemas");
+		
+		Professor professor = new Professor("Carlos Teles", "01001001-1", "010010010-01", "21-2345678", "carlosteles@firjan.com.br", "010110", "Programador de Sistemas");
+	}
+
+}
